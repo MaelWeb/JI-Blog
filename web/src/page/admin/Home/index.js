61c7bb43cb2c23eacb57e7252cd2c397e33ffbb9
Mael.Liang <mael.liang@live.com>
2017-10-27 18:42:09 +0800

    [update] home

--- web/src/page/admin/Home/index.js
+++ web/src/page/admin/Home/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-);
+import React, { Component } from 'react';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Content, Sider } = Layout;
 
-export default Home;
+import './index.less';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: false
+        };
+    }
+
+    onCollapse = (collapsed) => {
+        this.setState({ collapsed });
+    }
+
+    render() {
+        return(<div className="homg-layout"><Layout className='home-wrap'>
+            <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} className='home-sider' >
+                <div className="logo" />
+                <Menu mode="inline" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">
+                        <Icon type="user" />
+                        <span className="nav-text">nav 1</span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="video-camera" />
+                        <span className="nav-text">nav 2</span>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                      <Icon type="upload" />
+                      <span className="nav-text">nav 3</span>
+                    </Menu.Item>
+                    <Menu.Item key="4">
+                      <Icon type="bar-chart" />
+                      <span className="nav-text">nav 4</span>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout style={{ marginLeft: this.state.collapsed ? 75 : 200 }} className='home-content-layout'>
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                      ...
+                      <br />
+                      Really
+                      <br />...<br />...<br />...<br />
+                      long
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />...
+                      <br />...<br />...<br />...<br />...<br />...<br />
+                      content
+                    </div>
+                </Content>
+            </Layout>
+    </Layout></div>)
+    }
+}
