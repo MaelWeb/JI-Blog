07a7b701ee191c2f078ee8a1a28da23d03780e23
Mael.Liang <mael.liang@live.com>
2017-12-12 17:59:54 +0800

    [update] fix comment ui

--- web/page/blog/index.js
+++ web/page/blog/index.js
@@ -4,6 +4,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import './index.less';
 
+document.addEventListener('DOMContentLoaded', ready, false);
+
+document.addEventListener('touchmove', function(ev) {
+    ev.preventDefault();
+}, false);
+
+function ready() {
+    document.querySelector('.page-main').addEventListener('touchmove', function(ev) {
+        ev.stopPropagation();
+    }, false);
+}
+
 ReactDOM.hydrate((
     <BrowserRouter >
         <App InitData={window._SERVER_DATA} />
