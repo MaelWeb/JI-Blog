d50013d4d2edca0354005986b1ab030ebd983e2a
Mael.Liang <mael.liang@live.com>
2017-12-18 18:54:03 +0800

    [Update] 线上打包脚本

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
+var baseWebpackConfig = require('./webpack.base.config');
+var Html = require('html-webpack-plugin');
+const templateSrc = path.join(__dirname, '../web/page/');
+const outputPath = path.join(__dirname, '../output/client/');
+
+module.exports = merge(baseWebpackConfig, {
+    devtool: 'source-map',
+    entry: {
+        admin: '../web/page/admin/index.js',
+        blog: '../web/page/blog/index.js',
+        vendor: ['react', 'react-dom', 'axios', 'classnames']
+    },
+    output: {
+        path: outputPath,
+        publicPath: 'http://oyfapuy1b.bkt.clouddn.com/',
+        filename: 'js/[name]_[chunkhash:8].js',
+    },
+    plugins: [
+
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+                DEBUG: false
+            }
+        }),
+        new Html({
+            filename: 'admin.html',
+            template: path.join(templateSrc, '/admin/index.html'),
+            chunks: ["vendors", "admin"],
+        }),
+        new Html({
+            filename: 'blog.html',
+            template: path.join(templateSrc, '/blog/index.html'),
+            chunks: ["vendors", "blog"],
+            html: '<%- html %>',
+            script: '<%- JSON.stringify(ServerData) %>',
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            // 最紧凑的输出
+            beautify: false,
+            // 删除所有的注释
+            comments: false,
+            compress: {
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false,
+                // 删除所有的 `console` 语句
+                // 还可以兼容ie浏览器
+                drop_console: true,
+                // 内嵌定义了但是只用到一次的变量
+                collapse_vars: true,
+                // 提取出出现多次但是没有定义成变量去引用的静态值
+                reduce_vars: true,
+            }
+        })
+    ]
+});
--- server/index.js
+++ server/index.js
@@ -4,51 +4,66 @@ import mongoose from 'mongoose';
 import koaStatic from 'koa-static';
 import views from 'koa-views';
 import path from 'path';
-import webpack from 'webpack';
-import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware';
-import devConfig from '../build/webpack.dev.config';
-import BaseConfig from '../build/webpack.base.config.js';
-import SourceMapSupport from 'source-map-support';
 
 import _Config from './config';
 import Router from './router';
 
-// SourceMapSupport.install();
 
 const App = new koa();
-const compile = webpack(devConfig);
+
+const NODE_ENV = process.env.NODE_ENV;
+
+// 开发环境启动webpack编译
+if (NODE_ENV == 'development') {
+    var webpack = require('webpack');
+    var { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
+    var devConfig = require('../build/webpack.dev.config');
+
+    const compile = webpack(devConfig);
+    App.use(devMiddleware(compile, {
+        // watch options (only lazy: false)
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: true
+        },
+        // public path to bind the middleware to
+        // use the same as in webpack
+        publicPath: devConfig.output.publicPath,
+
+        // options for formating the statistics
+        stats: {
+            colors: true,
+            chunks: false,
+        },
+        debug: true,
+        lazy: false,
+        historyApiFallback: true
+    }))
+
+    App.use(hotMiddleware(compile, {
+        // log: console.log,
+        // path: '/__webpack_hmr',
+        // heartbeat: 10 * 1000
+    }))
+}
+
 
 mongoose.Promise = Promise;
 mongoose.connect(_Config.mongodb.url, _Config.mongodbSOptions);
 mongoose.connection.on('error', console.error);
 
-App.use(devMiddleware(compile, {
-    // watch options (only lazy: false)
-    watchOptions: {
-        aggregateTimeout: 300,
-        poll: true
-    },
-    // public path to bind the middleware to
-    // use the same as in webpack
-    publicPath: BaseConfig.output.publicPath,
-
-    // options for formating the statistics
-    stats: {
-        colors: true
-    }
-}))
-
-App.use(hotMiddleware(compile, {
-    // log: console.log,
-    // path: '/__webpack_hmr',
-    // heartbeat: 10 * 1000
-}))
-
 // 配置服务端模板渲染引擎中间件
-App.use(views(path.join(__dirname, '../output/views'), {
-    extension: 'html',
-    map: { html: 'ejs' }
-}))
+// if (NODE_ENV == 'development') {
+//     App.use(views(path.join(__dirname, '../output/devViews'), {
+//         extension: 'html',
+//         map: { html: 'ejs' }
+//     }))
+// } else {
+    App.use(views(path.join(__dirname, '../output/client'), {
+        extension: 'html',
+        map: { html: 'ejs' }
+    }))
+// }
 
 // 配置静态资源加载中间件
 App.use(koaStatic(
@@ -71,7 +86,7 @@ App.listen(_Config.port, () => {
 
 
 function pageNotFound() {
-    return async (ctx, next) => {
+    return async(ctx, next) => {
         let response = ctx.response;
         if (response.status == 404) {
             ctx.render('404');
