17b0613a70bb30ef7463e3f8679eb4a73d1e6939
Mael.Liang <mael.liang@live.com>
2018-01-18 11:45:57 +0800

    [Update] baidu push

--- web/page/blog/App/index.js
+++ web/page/blog/App/index.js
@@ -50,6 +50,18 @@ class App extends Component {
 
     }
 
+    componentDidMount() {
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        } else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+    }
+
     componentDidUpdate(prevProps, prevState) {
         if ((this.props.location !== prevProps.location)) {
             window.scrollTo(0, 0);
