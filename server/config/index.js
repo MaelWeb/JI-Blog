42cad0d1241eaf7777c743b1b1852457369698a8
liangzhongling <mael.liang@live.com>
2018-02-27 23:08:18 +0800

    [Update] 后台修复

--- server/config/index.js
+++ server/config/index.js
@@ -12,14 +12,14 @@ let config = {
         useMongoClient: true
     },
     upload: {
-        ACCESS_KEY: 'v6NxwNQtqVpK0Z51X0iq-YXv0Fo8qlFcVxnlYbO6',
-        SECRET_KEY: 'b5p89rqZeO8KJEjEvOp3Pk2fHtKEToE5PdSmQLRq'
+        ACCESS_KEY: 'v6NxwNQtqVpK0Z******YXv0Fo8qlFcVxnlYbO6',
+        SECRET_KEY: 'b5p89rqZeO8KJE******k2fHtKEToE5PdSmQLRq'
     },
     InvitationCode: "5620",
     wechat: {
         host: 'https://api.weixin.qq.com',
-        appId: 'wx05c5964f92033acc',
-        secret: '0cf16dbd475ac26d03a3e1a6d0db04e6'
+        appId: 'wx05c596******2033acc',
+        secret: '0cf16dbd475******03a3e1a6d0db04e6'
     }
 }
 // 可在private.js定义自己私有的配置
