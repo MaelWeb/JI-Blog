06e272f2b860e9b9efa1068ce9261496b3890003
liangzhongling <mael.liang@live.com>
2018-02-27 23:07:49 +0800

    [Update] 后台修复

--- server/config/index.js
+++ server/config/index.js
@@ -2,24 +2,24 @@ const path = require('path');
 let config = {
     port: process.env.PORT || 8080,
     mongodb: {
-        url: 'mongodb://root:root@127.0.0.1:27017/koa-blog'
+        url: 'mongodb://root:mael147863@127.0.0.1:27017/blog'
     },
     jwt: {
-        secret: "test" //默认
+        secret: 'MAELLIANG_BLOG' //默认
     },
     mongodbSOptions: { //mongodb用户和密码
         authSource: "admin",
         useMongoClient: true
     },
     upload: {
-        ACCESS_KEY: 'v6NxwNQtqVpK0Z51*****v0Fo8qlFcVxnlYbO6',
-        SECRET_KEY: 'b5p89rqZeO8KJE*****2fHtKEToE5PdSmQLRq'
+        ACCESS_KEY: 'v6NxwNQtqVpK0Z51X0iq-YXv0Fo8qlFcVxnlYbO6',
+        SECRET_KEY: 'b5p89rqZeO8KJEjEvOp3Pk2fHtKEToE5PdSmQLRq'
     },
-    InvitationCode: "1111",
+    InvitationCode: "5620",
     wechat: {
         host: 'https://api.weixin.qq.com',
-        appId: 'wx05c5*****92033acc',
-        secret: '0cf16dbd*****26d03a3e1a6d0db04e6'
+        appId: 'wx05c5964f92033acc',
+        secret: '0cf16dbd475ac26d03a3e1a6d0db04e6'
     }
 }
 // 可在private.js定义自己私有的配置
