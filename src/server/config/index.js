c969fb66e07dccaa8027a848d8324ecae0872ff4
mael <mael.liang@live.com>
2020-06-10 23:11:58 +0800

    bug: 文章编辑

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -1,34 +1,33 @@
-const path = require("path")
+const path = require('path');
 let config = {
     port: process.env.PORT || 8080,
     mongodb: {
-        url: "mongodb://root:*******@127.0.0.1:27017/blog",
+        url: 'mongodb://root:j******@127.0.0.1:27017/blog'
     },
     jwt: {
-        secret: "*********", //默认
+        secret: 'MAELLIANG_BLOG' //默认
     },
-    mongodbSOptions: {
-        //mongodb用户和密码
+    mongodbSOptions: { //mongodb用户和密码
         authSource: "admin",
-        useMongoClient: true,
+        useMongoClient: true
     },
     upload: {
-        ACCESS_KEY: "v6NxwNQtq********Fo8qlFcVxnlYbO6",
-        SECRET_KEY: "b5p89rqZeO8KJ********HtKEToE5PdSmQLRq",
+        ACCESS_KEY: 'v6NxwNQtq******@-YXv0Fo8qlFcVxnlYbO6',
+        SECRET_KEY: 'b5p89rqZeO******@HtKEToE5PdSmQLRq'
     },
-    geetest: {
-        ID: "108814b3c95*******8d988bc76d12",
-        KEY: "48b9103ef********9ceabd6e08c",
+   geetest: {
+        ID: "108814b3c******@d988bc76d12",
+        KEY: "48b9103e******@6679ceabd6e08c"
     },
-    InvitationCode: "****",
+    InvitationCode: "1123",
     wechat: {
-        host: "https://api.weixin.qq.com",
-        appId: "wx05c59****033acc",
-        secret: "0cf16dbd47*****a3e1a6d0db04e6",
-    },
+        host: 'https://api.weixin.qq.com',
+        appId: 'wx05******@2033acc',
+        secret: '0cf16******@3e1a6d0db04e6'
+    }
 }
 // 可在private.js定义自己私有的配置
 // if (fs.existsSync(__dirname + '/private.js')) {
 //     config = Object.assign(config, require('./private.js'));
 // }
-module.exports = config
+module.exports = config;
