f2e36e4fae1ad6cbb013d46c99401ca104199851
lianzhongling <lianzhongling@yy.com>
2020-08-10 11:19:40 +0800

    feat: 性能检测

--- src/client/page/blog/index.js
+++ src/client/page/blog/index.js
@@ -3,6 +3,7 @@ import 'core-js/es6/set';
 import 'core-js/es6/promise';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import wpkReporter from 'wpk-reporter'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.less';
@@ -27,4 +28,11 @@ if (process.env.NODE_ENV === 'development' && module.hot && !isFirstRender) {
         document.getElementById('app'),
     );
     isFirstRender = false;
+
+    const __wpk = new wpkReporter({
+        bid: 'ji-blog-niivg1a5', // 新建应用时确定
+        spa: true, // 单页应用开启后，可更准确地采集PV
+        plugins: []
+    });
+    __wpk.installAll(); // 初始化sdk 必须调用
 }
--- src/server/config/index.js
+++ src/server/config/index.js
@@ -2,28 +2,28 @@ const path = require('path');
 let config = {
     port: process.env.PORT || 8080,
     mongodb: {
-        url: 'mongodb://root:j******@127.0.0.1:27017/blog'
+        url: 'mongodb://root:8888888@127.0.0.1:27017/blog'
     },
     jwt: {
-        secret: 'MAELLIANG_BLOG' //默认
+        secret: '88888' //默认
     },
     mongodbSOptions: { //mongodb用户和密码
         authSource: "admin",
         useMongoClient: true
     },
     upload: {
-        ACCESS_KEY: 'v6NxwNQtq******@-YXv0Fo8qlFcVxnlYbO6',
-        SECRET_KEY: 'b5p89rqZeO******@HtKEToE5PdSmQLRq'
+        ACCESS_KEY: '999999',
+        SECRET_KEY: '9999999'
     },
    geetest: {
-        ID: "108814b3c******@d988bc76d12",
-        KEY: "48b9103e******@6679ceabd6e08c"
+        ID: "999",
+        KEY: "9999"
     },
-    InvitationCode: "1123",
+    InvitationCode: "iiiiii",
     wechat: {
         host: 'https://api.weixin.qq.com',
-        appId: 'wx05******@2033acc',
-        secret: '0cf16******@3e1a6d0db04e6'
+        appId: '000',
+        secret: '99999'
     }
 }
 // 可在private.js定义自己私有的配置
